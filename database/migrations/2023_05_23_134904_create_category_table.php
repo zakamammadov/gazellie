f23f7a9a87aa3ea55a73a270aada1f492a8e96df
zakamamadov <zakamammadov18@gmail.com>
2023-05-23 18:03:24 +0400

    8 th commit

---
+++ database/migrations/2023_05_23_134904_create_category_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->id();
+            $table->string('cat_name_az')->nullable();
+            $table->string('cat_name_en')->nullable();
+
+            $table->string('cat_name_ru')->nullable();
+
+            $table->string('slug')->nullable();
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category');
+    }
+}
